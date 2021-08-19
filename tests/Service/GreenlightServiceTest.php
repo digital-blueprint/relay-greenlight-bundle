d8ebb770220a289cbc7801e1479b3d8e5e4f4c3e
Patrizio Bekerle <patrizio@bekerle.com>
2021-08-19 15:15:11 +0200

    Fetch photo of person with permit

--- tests/Service/GreenlightServiceTest.php
+++ tests/Service/GreenlightServiceTest.php
@@ -6,6 +6,7 @@ namespace Dbp\Relay\GreenlightBundle\Tests\Service;
 
 use DBP\API\BaseBundle\Entity\Person;
 use DBP\API\BaseBundle\TestUtils\DummyPersonProvider;
+use Dbp\Relay\GreenlightBundle\API\PersonPhotoProviderInterface;
 use Dbp\Relay\GreenlightBundle\Service\GreenlightService;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
@@ -27,12 +28,13 @@ class GreenlightServiceTest extends WebTestCase
         $personProvider = new DummyPersonProvider($person);
         $entityManager = $this->createMock(EntityManagerInterface::class);
         $managerRegistry = $this->createMock(ManagerRegistry::class);
+        $personPhotoProviderInterface = $this->createMock(PersonPhotoProviderInterface::class);
 
         $managerRegistry->expects($this->any())
             ->method('getManager')
             ->willReturnOnConsecutiveCalls($entityManager);
 
-        $this->greenlightService = new GreenlightService($personProvider, $managerRegistry);
+        $this->greenlightService = new GreenlightService($personProvider, $managerRegistry, $personPhotoProviderInterface);
     }
 
     public function testNothing()
