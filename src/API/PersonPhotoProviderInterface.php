027d73fdda5049dac54d0e37e4c1319e91ec0ffd
Patrizio Bekerle <patrizio@bekerle.com>
2021-08-19 11:44:21 +0200

    Add PersonPhotoProviderInterface

---
+++ src/API/PersonPhotoProviderInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\GreenlightBundle\API;
+
+use DBP\API\BaseBundle\Entity\Person;
+
+interface PersonPhotoProviderInterface
+{
+    public function getPhotoData(Person $person): ?string;
+}
