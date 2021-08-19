1f58866d950850fab44c0f629da4d1b7620fa2f8
Christoph Reiter <reiter.christoph@gmail.com>
2021-08-19 17:25:37 +0200

    Move the assets into the source tree
    So we can use them outside of tests.
    
    Also adds an image for missing photos and some license information.

---
+++ src/Assets/README.md
@@ -0,0 +1,9 @@
+example_photo.jpeg
+
+    Source: https://commons.wikimedia.org/wiki/File:Erika_Mustermann_2010.jpg?uselang=de
+    License: public domain
+
+sourcesanspro.ttf
+
+    Source: https://github.com/adobe-fonts/source-sans
+    License: SIL Open Font License 1.1
--- tests/VizHashTest.php
+++ tests/VizHashTest.php
@@ -11,8 +11,8 @@ class VizHashTest extends TestCase
 {
     public function testGenerateVizHash()
     {
-        $photoData = file_get_contents(__DIR__.'/erika.jpg');
-        $font = __DIR__.'/sourcesanspro.ttf';
+        $photoData = file_get_contents(__DIR__.'/../src/Assets/example_photo.png');
+        $font = __DIR__.'/../src/Assets/sourcesanspro.ttf';
 
         $jpeg = VizHash::create('foobar', $photoData, 600, 'Erika Musterfrau 1970', $font, 80);
         $this->assertNotNull($jpeg);
