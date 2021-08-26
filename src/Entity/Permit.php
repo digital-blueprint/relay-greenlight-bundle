1c7e1dfa31bc724510ba8ceff1bdc99469628b29
Patrizio Bekerle <patrizio@bekerle.com>
2021-08-26 15:42:27 +0200

    Use "local-proof" in example

--- src/Entity/Permit.php
+++ src/Entity/Permit.php
@@ -19,7 +19,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
  *                     "content" = {
  *                         "application/json" = {
  *                             "schema" = {"type" = "object"},
- *                             "example" = {"consentAssurance" = true, "additionalInformation" = ""}
+ *                             "example" = {"consentAssurance" = true, "additionalInformation" = "local-proof"}
  *                         }
  *                     }
  *                 }
