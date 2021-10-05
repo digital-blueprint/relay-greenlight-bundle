7f0577d1e7ecfe91fa8e87f872d814ae040c513e
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2021-10-05 09:02:15 +0200

    Add PhotoServiceNotAvailableException

--- src/API/PersonPhotoProviderInterface.php
+++ src/API/PersonPhotoProviderInterface.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Dbp\Relay\GreenlightBundle\API;
 
 use DBP\API\BaseBundle\Entity\Person;
+use Dbp\Relay\GreenlightBundle\Exception\PhotoServiceNotAvailableException;
 
 interface PersonPhotoProviderInterface
 {
     /**
      * Returns the photo of a person as binary data.
+     *
+     * @throws PhotoServiceNotAvailableException
      */
     public function getPhotoData(Person $person): string;
 }
---
+++ src/Exception/PhotoServiceNotAvailableException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dbp\Relay\GreenlightBundle\Exception;
+
+class PhotoServiceNotAvailableException extends \Exception
+{
+}
