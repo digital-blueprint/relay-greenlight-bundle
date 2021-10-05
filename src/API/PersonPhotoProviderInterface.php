f93ef966a414606ff295b27dc65d938939362fa5
Patrizio Bekerle <patrizio.bekerle@tugraz.at>
2021-10-05 10:48:51 +0200

    Switch to PhotoServiceException

--- src/API/PersonPhotoProviderInterface.php
+++ src/API/PersonPhotoProviderInterface.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Dbp\Relay\GreenlightBundle\API;
 
 use DBP\API\BaseBundle\Entity\Person;
-use Dbp\Relay\GreenlightBundle\Exception\PhotoServiceNotAvailableException;
+use Dbp\Relay\GreenlightBundle\Exception\PhotoServiceException;
 
 interface PersonPhotoProviderInterface
 {
     /**
      * Returns the photo of a person as binary data.
      *
-     * @throws PhotoServiceNotAvailableException
+     * @throws PhotoServiceException
      */
     public function getPhotoData(Person $person): string;
 }
--- src/Exception/PhotoServiceException.php
+++ src/Exception/PhotoServiceException.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\GreenlightBundle\Exception;
 
-class PhotoServiceNotAvailableException extends \Exception
+class PhotoServiceException extends \Exception
 {
 }
